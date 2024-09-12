e649109014ae8475ef4be3f162ad2ecab39cdbd8
kishore <skill2automate@gmail.com>
2024-09-12 09:51:20 +0530

    new chnages

--- Zproject003/src/test/java/tests/Sample1.java
+++ Zproject003/src/test/java/tests/Sample1.java
@@ -2,7 +2,11 @@ package tests;
 
 public class Sample1 {
 	
+<<<<<<< HEAD
 	public void m1211() {
+=======
+	public void m1() {
+>>>>>>> parent of cfd7fa6 (3 nw methods added)
 	}
 	public void m222() {
 	}
